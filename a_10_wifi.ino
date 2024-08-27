ba5073588123c5465ef13ac098301adf02ae1418
איילה כהן <131193595+LielAyala@users.noreply.github.com>
2024-08-27 18:43:36 +0300

    Add files via upload
    הגדרת WiFi ושרת ה-WiFi

--- a_10_wifi.ino
+++ a_10_wifi.ino
@@ -3,50 +3,18 @@
 
 IPAddress apIP(55, 55, 55, 55); // הגדרת כתובת IP של נקודת הגישה
 
-const char* src_ssid = "Table"; //שם הרשת
-ESP8266WebServer server(80); 
+const char* src_ssid = "Table"; // שם הרשת של נקודת הגישה
+ESP8266WebServer server(80); // יצירת אובייקט שרת על פורט 80
+
 void WifiSetup(){
-  WiFi.mode(WIFI_AP_STA); 
-  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0)); 
-  WiFi.softAP(src_ssid);
-  server.on("/", handleRoot); 
-  server.onNotFound(handleNotFound); 
-  server.begin();
+  WiFi.mode(WIFI_AP_STA); // הגדרת המודול למצב נקודת גישה (AP) ותחנת רשת (STA) במקביל
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));   // הגדרת תצורת רשת ה-WiFi של נקודת הגישה   
+  WiFi.softAP(src_ssid); // יצירת נקודת גישה עם שם הרשת שהוגדר
+  server.on("/", handleRoot); // טיפול בבקשה לכתובת הבסיסית של השרת
+  server.onNotFound(handleNotFound); // טיפול בבקשות שאינן נמצאות
+  server.begin(); // התחלת עבודת השרת
 }
 
 void WifiServer_loop() {
-  server.handleClient(); 
-  delay(10); // המתנה קצרה בין בדיקות
-} 
-
-
-void handleRoot() {
-  String html = "<html><head><title>Performance Table</title></head><body>";
-  html += "<center><h1>Your Performance Table</h1>"; // יצירת כותרת 
-  html += "<table border='1'><tr><th>Index</th><th>Duration</th><th>Is Improve</th></tr>";
-  for (int i = 0; i < recordIndex ; i++) {
-    html += "<tr><td>" + String(i + 1) + "</td><td>" + String(pressRecords[i]) + "</td><td>" + (isRecordNew[i] ? "Yes" : "No") + "</td></tr>"; // יצירת שורה בטבלה עבור כל לחיצה
-  }
-  html += "</table></center></body></html>"; // סיום יצירת הטבלה והדף
-
-  server.send(200, "text/html", html); // שליחת תוכן HTML ללקוח
+  server.handleClient(); // טיפול בבקשות HTTP המגיעות לשרת
 }
-
-// שליחת הודעת שגיאה 404 ללקוח
-void handleNotFound(){
-  String message = "File Not Found \n \n";
-  message += "URI: ";
-  message += server.uri(); 
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST"; 
-  message += "\nArguments: ";
-  message += server.args(); 
-  message += "\n";
-
-  for (uint8_t i = 0; i < server.args(); i++) {
-      message += " " + server.argName(i) + ": " + server.arg(i) + "\n"; 
-  }
-
-  server.send(404, "text/plain", message); 
-}
-
