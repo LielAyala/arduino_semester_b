da4f9e5b6a2a0cd8c6a47f801196bc981f335972
איילה כהן <131193595+LielAyala@users.noreply.github.com>
2024-08-27 18:44:40 +0300

    Add files via upload
    פונקציות לניהול הבקשות מהשרת

--- a_10_wifi.ino
+++ a_10_wifi.ino
@@ -18,3 +18,43 @@ void WifiSetup(){
 void WifiServer_loop() {
   server.handleClient(); // טיפול בבקשות HTTP המגיעות לשרת
 }
+
+
+
+
+
+void handleRoot() {
+  String html = "<html><head><title>Performance Table</title></head><body>";
+  html += "<center><h1>Your Performance Table</h1>"; // יצירת כותרת
+  html += "<table border='1'><tr><th>Index</th><th>Duration</th><th>Is Improve</th></tr>";
+  for (int i = 0; i < recordIndex ; i++) {
+    html += "<tr><td>" + String(i + 1) + "</td><td>" + String(pressRecords[i]) + "</td><td>" + (isRecordNew[i] ? "Yes" : "No") + "</td></tr>"; // יצירת שורה בטבלה עבור כל לחיצה
+  }
+  html += "</table></center></body></html>"; // סיום יצירת הטבלה והדף
+
+
+  server.send(200, "text/html", html); // שליחת תוכן HTML ללקוח
+}
+
+
+// שליחת הודעת שגיאה 404 ללקוח
+void handleNotFound(){
+  String message = "File Not Found \n \n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+
+
+  for (uint8_t i = 0; i < server.args(); i++) {
+      message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+
+
+  server.send(404, "text/plain", message);
+}
+
+
